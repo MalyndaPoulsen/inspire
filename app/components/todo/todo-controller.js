4e4c19da893965d25a28b6b0d5151a3e3bb31509
MaLynda Poulsen <poulsenjones@yahoo.com>
2016-05-02 22:42:48 -0600

    todo

--- app/components/todo/todo-controller.js
+++ app/components/todo/todo-controller.js
@@ -1,8 +1,31 @@
-app.controller('TodoController', function(TodoService){
-	var tc = this;
-	tc.todos = TodoService.getTodos();
-	// DO NOT EDIT ABOVE
-	
-	
-	
+// app.controller('TodoController', function(TodoService) {
+//     var tc = this;
+//     tc.todos = TodoService.getTodos();
+
+
+app.controller("TodoController", function() {
+    var tc = this;
+
+    // DO NOT EDIT ABOVE
+    tc.addTask = function(newTask) {
+        tc.todoList = [];
+        tc.todoList.push(newTask)
+        tc.newTask = '';
+        tc.taskComplete = 0;
+    }
+
+    tc.reset = function() {
+        tc.todoList = [];
+        tc.taskCompleted = 0;
+    }
+    tc.removeTask = function(index) {
+        tc.todoList.splice(index, 1);
+    }
+
+    //     $scope.showMe= false;
+    //    $scope.myFunc = function(){
+    //        $scope.showMe = !$scope.showMe;
+    // }
+
+
 })
